dfa18fa2c6110dcf4474a7e58d490e47d8200bf7
Ismail Taher <ismail.taher@ymail.com>
2025-10-21 19:40:59 +0500

    operators

---
+++ 3-operators/meaning.py
@@ -0,0 +1,12 @@
+meaning = 42
+print('')
+
+# if-else
+
+# if meaning > 10:
+#     print("right on!")
+# else:
+#     print("not today!")
+
+# Ternary Operator
+print('right on!') if meaning > 10 else print('not today')
