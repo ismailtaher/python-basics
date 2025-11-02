00ba87090cd688b4c0772bf7ffc7d17de6681c01
Ismail Taher <ismail.taher@ymail.com>
2025-11-02 19:11:47 +0500

    user input: rock paper scissor

---
+++ 5-user-input/rps.py
@@ -0,0 +1,42 @@
+import sys  # to exit game
+import random  # to randomly choose for computer
+from enum import Enum
+
+
+class RPS(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+
+
+print("")
+playerchoice = input(
+    'Enter... \n1 for Rock,\n2 for Paper,\n3 for Scissors:\n\n')
+
+# casting to int bcz val input by user is always sting data
+player = int(playerchoice)
+
+if player > 3 or player < 1:
+    sys.exit("You must enter a value between 1 & 3")
+
+# computer will randomly choose between these vals in the string i.e. 1, 2 or 3
+computerchoice = random.choice("123")
+
+# casting to number for computer choice
+computer = int(computerchoice)
+
+print("")
+print("You chose " + str(RPS(player)).replace('RPS.', '') + ".")
+print("Python chose " + str(RPS(computer)).replace('RPS.', '') + ".")
+print("")
+
+if player == 1 and computer == 3:
+    print("🥳 You win!")
+elif player == 2 and computer == 1:
+    print("🥳 You win!")
+elif player == 3 and computer == 2:
+    print("🥳 You win!")
+elif player == computer:
+    print("😮 Tie game!")
+else:
+    print("🐍 Python wins!")
