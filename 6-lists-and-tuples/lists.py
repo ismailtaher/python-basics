85572e1af88cfa021ecf7b5f0a61d325639b26a6
Ismail Taher <ismail.taher@ymail.com>
2025-11-03 21:58:25 +0500

    add more methods for lists

--- 6-lists-and-tuples/lists.py
+++ 6-lists-and-tuples/lists.py
@@ -74,9 +74,61 @@ users.insert(0, 'Bob')
 print(users)
 
 # insert using range to replace
+# x:x start & end at the same position
 users[2:2] = ["Eddie", "Alex"]
 print(users)
 
 #
 # replace method
+# will replace items in starting from x ending at y-1 in x:y
+users[1:3] = ['Robert', 'JPJ']
+print(users)
+
+
 #
+# remove method
+# remove a specific item from a list
+users.remove("Bob")
+print(users)
+
+#
+# pop method
+# will rmeove the last item from the list & also return that value
+print(users.pop())
+print(users)
+
+#
+# del keyword
+#
+
+del users[0]  # delete first item
+print(users)
+
+# delete the whole list
+# del data
+# print(data)
+
+
+#
+# clear method
+# empty or clear out the whole list, the list will still exist but with no items in it
+data.clear()
+print(data)
+
+
+users.pop()
+
+#
+# sort method
+#
+users[1:2] = ['dave']
+# will sort all items alphabetically, also first it sorts all the uppercase, then the lowercase words
+users.sort()
+print(users)
+
+# to include lowercase words in the sorting along with the uppercase/propercase ones
+# now dave will be sorted alphabetically along with all other items
+users.sort(key=str.lower)
+print(users)
+
+# NOTE: it will not sort between different data types, will throw error in that case
